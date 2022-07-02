344058e6acadf5e33d0c428ed53371f7f471f1bb
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-07-02 07:51:47 +0700

    Initial Commit

---
+++ CatChecker/CatCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCheckerApp.swift
+//  CatChecker
+//
+//  Created by Piotr Woźniak on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
